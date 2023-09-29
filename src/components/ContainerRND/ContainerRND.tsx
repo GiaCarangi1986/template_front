fb5371a575f61a3b2ab1ecb5c6bcddb7c1e81b34
GiaCarangi1986 <dasha.71.99@mail.ru>
2023-09-29 19:39:03 +0300

    dev-rnd blocks стилизация компонентов + delete icon btn add clickable btns (add delete with header btns)

--- src/components/ContainerRND/ContainerRND.tsx
+++ src/components/ContainerRND/ContainerRND.tsx
@@ -1,72 +1,76 @@
-import { FC, useState } from 'react'
-import { Rnd } from 'react-rnd'
+import {FC, useState} from 'react'
+import {Rnd} from 'react-rnd'
 import cn from 'classnames'
 
-import { CommonConst } from '../../const'
-import { setZIndex } from '../../utils/rnd'
+import {CommonConst} from '../../const'
+import {setZIndex} from '../../utils/rnd'
 import HeaderRND from './HeaderRND'
+import {useAppSelector} from "../../store/hooks";
 
-import { ContainerRNDProps, BlockRndType } from './types'
+import {ContainerRNDProps, BlockRndType} from './types'
 import style from './index.module.scss'
 
 const INIT_RECT: BlockRndType = {
-  x: 0,
-  y: 0,
-  width: '100%',
-  height: '100%'
+    x: 0,
+    y: 0,
+    width: '100%',
+    height: '100%'
 }
 
 const ContainerRND: FC<ContainerRNDProps> = ({
-  children,
-  bounds,
-  name,
-  headerLabel,
-  variant
-}) => {
-  const [block, setBlock] = useState<BlockRndType>(INIT_RECT)
-  const [active, setActive] = useState(false)
+                                                 children,
+                                                 bounds,
+                                                 name,
+                                                 headerLabel,
+                                                 variant
+                                             }) => {
+    const [block, setBlock] = useState<BlockRndType>(INIT_RECT)
+    const [active, setActive] = useState(false)
+    const handleActive = () => setActive(!active)
 
-  const handleActive = () => setActive(!active)
+    const btns = useAppSelector((state) => state.mainWidgets)
+    const modal = btns.find(el => el.name === name)
 
-  return (
-    <Rnd
-      default={INIT_RECT}
-      className={cn(CommonConst.CLASSNAME.containerRND, style.container, {
-        [style.container_active]: active,
-        [style[`container_${variant}`]]: variant
-      })}
-      onResizeStart={(e, dir, data) => {
-        data.style.zIndex = setZIndex()
-      }}
-      onDragStart={(e, data) => {
-        data.node.style.zIndex = setZIndex()
-        handleActive()
-      }}
-      onDragStop={(e, d) => {
-        setBlock({ ...block, x: d.x, y: d.y })
-        handleActive()
-      }}
-      onResizeStop={(e, direction, ref, delta, position) => {
-        setBlock({
-          width: ref.style.width,
-          height: ref.style.height,
-          ...position,
-        })
-      }}
-      size={{ width: block.width, height: block.height }}
-      position={{ x: block.x, y: block.y }}
-      dragGrid={[5, 5]}
-      resizeGrid={[5, 5]}
-      bounds={`.${bounds}`}
-      dragHandleClassName={CommonConst.CLASSNAME.headerRND}
-      onMouseDown={() => console.log('onClick')}
-    >
-      <>
-        <HeaderRND label={headerLabel} />
-        <div className={style.body}>{children}</div>
-      </>
-    </Rnd>
-  )
+    return (
+        <Rnd
+            default={INIT_RECT}
+            className={cn(CommonConst.CLASSNAME.containerRND, style.container, {
+                [style.container_active]: active,
+                [style[`container_${variant}`]]: variant,
+                [style.hide]: !modal?.show
+            })}
+            onResizeStart={(e, dir, data) => {
+                data.style.zIndex = setZIndex()
+            }}
+            onDragStart={(e, data) => {
+                data.node.style.zIndex = setZIndex()
+                handleActive()
+            }}
+            onDragStop={(e, d) => {
+                setBlock({...block, x: d.x, y: d.y})
+                handleActive()
+            }}
+            onResizeStop={(e, direction, ref, delta, position) => {
+                setBlock({
+                    width: ref.style.width,
+                    height: ref.style.height,
+                    ...position,
+                })
+            }}
+            size={{width: block.width, height: block.height}}
+            position={{x: block.x, y: block.y}}
+            dragGrid={[5, 5]}
+            resizeGrid={[5, 5]}
+            bounds={`.${bounds}`}
+            dragHandleClassName={CommonConst.CLASSNAME.headerRND}
+            onMouseDown={() => console.log('onClick')}
+        >
+            <>
+                <HeaderRND label={headerLabel}/>
+                <div className={style.body}>{children}</div>
+            </>
+        </Rnd>
+    )
 }
 
 export default ContainerRND
--- src/components/PageConstructor/Header/RndButtons/RndButtons.tsx
+++ src/components/PageConstructor/Header/RndButtons/RndButtons.tsx
@@ -1,25 +1,48 @@
-import { Button, Icon } from '../../../../views/common'
-import { BlockModuleConst } from '../../../../const'
-import { DefaultStateIcon } from '../../../../images'
-import { useAppSelector } from '../../../../store/hooks'
+import {Button, Icon} from '../../../../views/common'
+import {BlockModuleConst} from '../../../../const'
+import {DefaultStateIcon} from '../../../../images'
+import {useAppDispatch, useAppSelector} from '../../../../store/hooks'
+import {mainWidgetsStore} from '../../../../store/reducer/mainWidgets'
 
 import style from './index.module.scss'
 
 const RndButtons = () => {
-  const btns = useAppSelector((state: any) => state.mainWidgets)
-  return (
-    <div className={style.rnd_btns}>
-      <div className={style.rnd_btns__block}>
-        {Object.keys(BlockModuleConst.BLOCKS).map(el => {
-          const elem = BlockModuleConst.BLOCKS[el]
-          return <Button key={elem.name} view='light' label={elem.btnLabel} />
-        })}
-      </div>
-      <Button view='light' iconView='light'>
-        <Icon><DefaultStateIcon /></Icon>
-      </Button>
-    </div>
-  )
+    const btns = useAppSelector((state) => state.mainWidgets)
+    const dispatch = useAppDispatch();
+    const {handleShow} = mainWidgetsStore.actions;
+
+    const handleClick = (name: string) => {
+        dispatch(handleShow(btns.map(btn => {
+            if (btn.name === name) {
+                return {
+                    ...btn,
+                    show: !btn.show
+                }
+            }
+            return btn
+        })))
+    }
+
+    return (
+        <div className={style.rnd_btns}>
+            <div className={style.rnd_btns__block}>
+                {Object.keys(BlockModuleConst.BLOCKS).map(el => {
+                    const elem = BlockModuleConst.BLOCKS[el]
+                    const itemBtn = btns.find(item => item.name === elem.name)
+                    const view = itemBtn?.show ? 'light' : 'dark'
+                    return <Button
+                        key={elem.name}
+                        view={view}
+                        label={elem.btnLabel}
+                        onClick={() => handleClick(elem.name)}
+                    />
+                })}
+            </div>
+            <Button view='light' iconView='light'>
+                <Icon><DefaultStateIcon/></Icon>
+            </Button>
+        </div>
+    )
 }
 
 export default RndButtons
